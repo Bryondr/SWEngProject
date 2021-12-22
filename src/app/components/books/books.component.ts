81e8bfd90732ebeb9b600f905106974c72eb333f
Deyan Bora Çetin <deyan_bora@hotmail.com>
2021-12-22 13:22:09 +0300

    Refresh with Buket's commit
    New Update for whole Project

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -1,3 +1,5 @@
+import { ToastrService } from 'ngx-toastr';
+import { UserFavBooksService } from './../../services/user-fav-books.service';
 import { DataService } from './../../services/data.service';
 import { Author } from '../../models/author';
 
@@ -22,7 +24,9 @@ export class BooksComponent implements OnInit {
   
 
   constructor(private bookService:BookService,
-    private data: DataService, private router:ActivatedRoute) { }
+    private data: DataService, private router:ActivatedRoute,
+    private userFavBookService:UserFavBooksService,
+    private toastrService:ToastrService) { }
 
   ngOnInit(): void {
     this.data.currentMessage.subscribe(message => this.message = message)
@@ -36,11 +40,17 @@ export class BooksComponent implements OnInit {
      });
   }
 
-  resetMessage(){
-     this.message=""
-    
+  newMessage(){
+    this.data.changeMessage("")
   }
 
+  addToFavBooks(bookid:number){
+    //let bookfavv= Object.assign({},bookid)
+    this.userFavBookService.addToFavBooks(bookid).subscribe(response=>{
+      this.toastrService.success("Favorilere Eklendi")
+    })
+  }
+ 
 
 
 
